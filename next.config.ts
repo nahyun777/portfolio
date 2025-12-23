e1f3b2e1a88f7c9d27ebd69c266d790156d582cf
nahyun777 <dusmiss@naver.com>
2025-12-23 10:06:59 +0900

    1223

--- next.config.ts
+++ next.config.ts
@@ -2,21 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['picsum.photos', 'images.unsplash.com', 'cdn.pixabay.com', 'pixabay.com'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'cdn.pixabay.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'pixabay.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-    ],
+    domains: ['picsum.photos', 'images.unsplash.com'],
   },
 };
 
